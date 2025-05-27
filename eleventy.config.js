bcf1eab68dc46bb58765956bd4594057f669b94c
Jason <jasonawhite@gmail.com>
2025-05-26 21:15:29 -0400

    added functions dir to eleventy config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,7 @@
 export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/js");
+  eleventyConfig.addPassthroughCopy("./src/functions");
+
   return {
     dir: {
       input: "src",
